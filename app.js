021c65b23b6e17cbec33b3810533c71dae22fdf7
medine yavuz <medineyldr.21@gmail.com>
2023-09-28 13:14:31 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const buttons=document.getElementsByTagName("button");
+
+uzunluk=buttons.length;
+i=0;
+
+sonuç=document.getElementById("sonuç");
+
+for(i=0; i<uzunluk; i++){
+    buttons[i].onclick=hesapla;
+}
+
+function hesapla(){
+    let deger=this.innerHTML;
+
+    if(deger=="="){
+        try{
+            sonuç.value=eval(sonuç.value);
+        }catch(e){
+            sonuç.value="0";
+        }
+        return;
+    }
+
+    sonuç.value += deger;
+}
